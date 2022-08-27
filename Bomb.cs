49f2bab07303f35c8dba33cc4f5b6f00230340e8
José Ferrão <tentone@outlook.com>
2022-08-27 17:14:40 +0100

    Game objects

---
+++ Bomb.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+internal class Bomb : GameObject
+{
+    public override void Initialize(GraphicsDevice graphicsDevice)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void Render(GameTime time, SpriteBatch spriteBatch)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void Update(GameTime time)
+    {
+        throw new System.NotImplementedException();
+    }
+}
---
+++ Player.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+internal class Player : GameObject
+{
+    public override void Initialize(GraphicsDevice graphicsDevice)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void Render(GameTime time, SpriteBatch spriteBatch)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void Update(GameTime time)
+    {
+        throw new System.NotImplementedException();
+    }
+}
